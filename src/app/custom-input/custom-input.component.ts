5a44bbda1a767d9abf7b9e7afb4101198e360ad2
kondareddyyaramala <kondareddy.yaramala@gmail.com>
2020-02-11 15:17:04 -0600

    1 file modified, 3 added

---
+++ src/app/custom-input/custom-input.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-input',
+  templateUrl: './custom-input.component.html',
+  styleUrls: ['./custom-input.component.scss']
+})
+export class CustomInputComponent implements OnInit {
+
+  @Input() label: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
